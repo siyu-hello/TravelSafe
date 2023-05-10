a4beb546518eb64c9f5a253827ce2041a2fc5631
Ucharon <1303962785@qq.com>
2023-05-10 20:16:12 +0800

    feat: ✨ init

---
+++ back-end/src/test/java/cn/homyit/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.homyit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
